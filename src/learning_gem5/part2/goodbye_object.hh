1cb9c30b6668f3cb964a643ecc52529ee71f2dad
flyinghorse0510 <mhy@flyinghorse.top>
2023-09-06 16:57:09 +0800

    my first simobject

--- src/learning_gem5/part2/goodbye_object.hh
+++ src/learning_gem5/part2/goodbye_object.hh
@@ -1,31 +1,3 @@
-/*
- * Copyright (c) 2017 Jason Lowe-Power
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are
- * met: redistributions of source code must retain the above copyright
- * notice, this list of conditions and the following disclaimer;
- * redistributions in binary form must reproduce the above copyright
- * notice, this list of conditions and the following disclaimer in the
- * documentation and/or other materials provided with the distribution;
- * neither the name of the copyright holders nor the names of its
- * contributors may be used to endorse or promote products derived from
- * this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
- * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 #ifndef __LEARNING_GEM5_GOODBYE_OBJECT_HH__
 #define __LEARNING_GEM5_GOODBYE_OBJECT_HH__
 
@@ -40,13 +12,7 @@ namespace gem5
 class GoodbyeObject : public SimObject
 {
   private:
-    /**
-     * Fill the buffer with the next chunk of data
-     */
     void processEvent();
-
-    /// An event that wraps the above function
-    EventFunctionWrapper event;
 
     /**
      * Fills the buffer for one iteration. If the buffer isn't full, this
@@ -54,13 +20,15 @@ class GoodbyeObject : public SimObject
      */
     void fillBuffer();
 
-    /// The bytes processed per tick
+    EventWrapper<GoodbyeObject, &GoodbyeObject::processEvent> event;
+
+    /// The bytes processed per tick.
     float bandwidth;
 
-    /// The size of the buffer we are going to fill
+    /// The size of the buffer we are going to fill.
     int bufferSize;
 
-    /// The buffer we are putting our message in
+    /// The buffer we are putting our message in.
     char *buffer;
 
     /// The message to put into the buffer.
@@ -70,7 +38,7 @@ class GoodbyeObject : public SimObject
     int bufferUsed;
 
   public:
-    GoodbyeObject(const GoodbyeObjectParams &p);
+    GoodbyeObject(const GoodbyeObjectParams &params);
     ~GoodbyeObject();
 
     /**
@@ -82,6 +50,6 @@ class GoodbyeObject : public SimObject
     void sayGoodbye(std::string name);
 };
 
-} // namespace gem5
+}
 
 #endif // __LEARNING_GEM5_GOODBYE_OBJECT_HH__
